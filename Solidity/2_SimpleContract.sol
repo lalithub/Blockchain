42ce9f190fb0d1adf35ccba3c9201a8a6a732af6
Lalit Shanker Choudhary <51866797+lalithub@users.noreply.github.com>
2021-11-16 16:25:18 +0530

    Simple contract

---
+++ Solidity/2_SimpleContract.sol
@@ -0,0 +1,21 @@
+// SPDX-License-Identifier: Apache 2.0
+pragma solidity ^0.8.7;
+
+contract MyCounter {
+    uint public count;
+
+    // Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    // Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+
+    // Function to decrement count by 1
+    function dec() public {
+        count -= 1;
+    }
+}
